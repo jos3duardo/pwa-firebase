e69c679f79537fe2cb1b8feed6230db84fac692c
jos3duardo <jos3duardolopes@gmail.com>
2020-05-01 16:55:19 -0400

    screen choose of camera

--- src/uploader/photo.js
+++ src/uploader/photo.js
@@ -1,5 +1,14 @@
+require('./photo/style.scss')
 let template = require('./photo/template.html')
-document.querySelector('body').innerHTML += template
-export default function () {
+document.querySelector('body').innerHTML += template;
 
+document.getElementById('cancel-photo').addEventListener('click', function (e) {
+    e.preventDefault();
+    let photoModal = document.getElementById('photo')
+    photoModal.className = 'modal'
+})
+
+export default function () {
+    let photoModal = document.getElementById('photo')
+    photoModal.className = 'modal open'
 }
