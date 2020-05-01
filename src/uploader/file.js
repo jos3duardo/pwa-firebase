1017fa91bf5d7c2e7e518666b571cf8dc1977545
jos3duardo <jos3duardolopes@gmail.com>
2020-05-01 12:18:15 -0400

    make a request form send file

--- src/uploader/file.js
+++ src/uploader/file.js
@@ -1,8 +1,18 @@
+import {app} from "../firebase";
+
 export default function () {
     let fileInput = document.getElementById('file');
     fileInput.click()
     fileInput.addEventListener('change', function (e) {
-        console.log(e.target.files[0]);
+
+        const storareRef = app.storage().ref();
+
+        let fileRef = storareRef.child('files/1/' + e.target.files[0].name)
+        fileRef.put(e.target.files[0]).then((snapshot) => {
+            console.log(snapshot);
+
+        })
+
     })
 
 }
