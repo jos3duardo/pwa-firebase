1097235b13d451ecb0b95691656dcadd3fca45e3
jos3duardo <jos3duardolopes@gmail.com>
2020-05-01 15:27:16 -0400

    save file on storage drive

--- src/uploader/file.js
+++ src/uploader/file.js
@@ -12,15 +12,24 @@ export default function () {
     let fileInput = document.getElementById('file');
     fileInput.click()
     fileInput.addEventListener('change', function (e) {
-
         const storareRef = app.storage().ref();
-
         let fileRef = storareRef.child('files/1/' + path + e.target.files[0].name)
         fileRef.put(e.target.files[0]).then((snapshot) => {
-            console.log(snapshot);
-
+            let folderRef = app.database().ref('files/1' + path);
+            snapshot.ref.getDownloadURL().then(function(url) {
+                 folderRef.push({
+                     type: 'file',
+                     title: e.target.files[0].name,
+                     url: url
+                 })
+            })
         })
 
     })
+    function setUtl(url) {
+        console.log(url)
+
+        return url;
+    }
 
 }
