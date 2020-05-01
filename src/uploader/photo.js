d4c1c61c0b37b006b4ef477d2d75baaedae41b4b
jos3duardo <jos3duardolopes@gmail.com>
2020-05-01 17:16:47 -0400

    list camera

--- src/uploader/photo.js
+++ src/uploader/photo.js
@@ -8,6 +8,29 @@ document.getElementById('cancel-photo').addEventListener('click', function (e) {
     photoModal.className = 'modal'
 })
 
+navigator.mediaDevices.enumerateDevices().then((devices) => {
+    let options = []
+    devices.forEach((device) => {
+
+        if (device.kind === "videoinput"){
+            options.push(device)
+        }
+    });
+
+    let menuCamera = document.querySelector('#photo-choice .menu-camera')
+    let partialTemplate = menuCamera.innerHTML;
+    console.log(partialTemplate)
+    let html = '';
+    options.forEach( (device) => {
+        let label = device.label || 'Camera'
+        html += partialTemplate
+            .replace(/{{ id }}/g, device.deviceId)
+            .replace(/{{ label }}/g, label)
+    })
+
+    menuCamera.innerHTML = html;
+})
+
 export default function () {
     let photoModal = document.getElementById('photo')
     photoModal.className = 'modal open'
