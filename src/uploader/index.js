61589b307d5f1835a1a4be791d19d20dab54a744
jos3duardo <jos3duardolopes@gmail.com>
2020-05-01 11:56:32 -0400

    determine actions for type of upload

--- src/uploader/index.js
+++ src/uploader/index.js
@@ -7,7 +7,30 @@ export default {
         btnCollection.forEach((btn) => {
             btn.addEventListener('click', (e) => {
                 e.preventDefault()
-                console.log(e.target)
+                let element = e.target;
+
+                if (element.tagName === 'I'){
+                    element = e.target.parentElement;
+                }
+                if (element.tagName === 'A'){
+                    switch (element.dataset.uploadType){
+                        case 'file':
+                            console.log('faça o upload');
+                            break;
+                        case 'folder':
+                            console.log('cria doiretorio');
+                            break;
+                        case 'photo':
+                            console.log('tira foto');
+                            break;
+                        case 'audio':
+                            console.log('grava audio');
+                            break;
+                        case 'note':
+                            console.log('escreve nota');
+                            break;
+                    }
+                }
             })
         })
     }
