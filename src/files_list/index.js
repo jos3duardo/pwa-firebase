142e41a057c7a564a1d87730ed92502d1c23bd04
jos3duardo <jos3duardolopes@gmail.com>
2020-05-01 07:32:54 -0400

    add files dynamically

--- src/files_list/index.js
+++ src/files_list/index.js
@@ -1,6 +1,34 @@
 export default {
     template:require('./template.html'),
     action(){
-        console.log('done')
+        let data = [
+            {
+                type: 'folder-open',
+                title: 'Notas'
+            },
+            {
+                type: 'folder-open',
+                title: 'Fotos'
+            },
+            {
+                type: 'file',
+                title: 'Anotações.txt'
+            },
+            {
+                type: 'file',
+                title: 'Outro arquivo.txt'
+            }
+        ];
+
+        let partial = require('./partial.html');
+
+        let html = '';
+        for (let index in data){
+            html += partial
+                .replace('{{ title }}', data[index].title)
+                .replace('{{ type }}', data[index].type)
+        }
+
+        document.querySelector('#main .files').innerHTML = html
     }
 }
