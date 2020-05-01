0658bfae5a4ba37e72df3d9d6111aab7783342c7
jos3duardo <jos3duardolopes@gmail.com>
2020-04-30 23:29:48 -0400

    template finished

--- src/template/index.js
+++ src/template/index.js
@@ -11,9 +11,28 @@ module.exports = {
         <div id="wrapper">
             ${header}${content}${footer}
         </div>
-        ${sidebar}
-    `,
+    ${sidebar}`,
     action: () => {
-        console.log('done');
+        let openMenu = document.querySelector('#header .menu-icon');
+        let closeMenu = document.getElementById('wrapper');
+
+        let closeMenuEvent = function (e) {
+            e.preventDefault();
+            let body = document.querySelector('body');
+            body.className = body.className.replace('show-menu','');
+
+            closeMenu.removeEventListener('click', closeMenuEvent, true);
+        };
+
+        let openMenuEvent = function (e) {
+            e.preventDefault();
+            let body = document.querySelector('body');
+            body.className += "show-menu";
+            body.className = body.className.trim()
+
+            closeMenu.addEventListener('click',  closeMenuEvent,true);
+        }
+
+        openMenu.addEventListener('click', openMenuEvent);
     }
 };
