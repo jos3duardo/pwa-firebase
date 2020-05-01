7d20fa58b2f7416de80524929d2c8c0f6facf281
jos3duardo <jos3duardolopes@gmail.com>
2020-05-01 19:13:14 -0400

    recorder audio

--- src/uploader/audio.js
+++ src/uploader/audio.js
@@ -1,11 +1,33 @@
+import uploader from "./utils/uploader";
+
 let streamAudio;
+let recorder;
+
+const audioRecorder = function () {
+    recorder = new MediaRecorder(streamAudio)
+    let chunks = [];
+    recorder.ondataavailable = (event) => {
+        chunks.push(event.data)
+    }
+
+    recorder.onstop = () => {
+        let blob = new Blob(chunks, { type: 'video/webm'})
+        chunks = []
+
+        let name = Math.random().toString(36).substring(2)
+        name += '.ogg';
+
+        uploader(blob, name)
+    }
+    recorder.start()
+}
 
 export default function () {
 
     if (streamAudio){
         streamAudio.getTracks()[0].stop()
+        recorder.stop();
         console.log('parando de reproduzindo audio')
-
     }else{
         let config = {
             video: false,
@@ -13,9 +35,9 @@ export default function () {
         }
         let success = function (stream) {
             streamAudio = stream;
+            audioRecorder()
             console.log('reproduzindo audio')
         }
         navigator.getUserMedia(config, success, (err) => console.log(err))
     }
-
 }
