85e79341bbe79f5160fb3560a98017150698b581
jos3duardo <jos3duardolopes@gmail.com>
2020-05-01 22:09:11 -0400

    save annotation in disk

--- src/uploader/note.js
+++ src/uploader/note.js
@@ -1,5 +1,9 @@
+import uploader from "./utils/uploader";
+
+require('./notes/style.scss')
+
 let template = document.createElement('template')
-template.innerHTML = require('./node/template.html')
+template.innerHTML = require('./notes/template.html')
 template = template.content.childNodes;
 document.querySelector('body').appendChild(template[0])
 
@@ -8,6 +12,22 @@ document.getElementById('note-cancel').addEventListener( 'click', (e) =>{
     let noteModal = document.getElementById('note')
     noteModal.className = 'modal'
 } )
+
+document.getElementById('note-save').addEventListener('click', function (e) {
+    e.preventDefault()
+    let data = [document.getElementById('note-text').value];
+    let blob = new Blob(data, {
+        encoding: 'UTF-8',
+        type: 'text/plain;charset=UTF-8'
+    })
+
+    let name = Math.random().toString(36).substring(2)
+    name += '.txt';
+
+    uploader(blob, name)
+    document.getElementById('note-cancel').click()
+})
+
 export default function () {
     let noteModal = document.getElementById('note')
     noteModal.className = 'modal open'
