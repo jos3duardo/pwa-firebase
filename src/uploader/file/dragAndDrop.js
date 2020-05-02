eb64fc3037834e4729ab6957400c4a87ec07475e
jos3duardo <jos3duardolopes@gmail.com>
2020-05-01 22:32:32 -0400

    dragover and dragleave

--- src/uploader/file/dragAndDrop.js
+++ src/uploader/file/dragAndDrop.js
@@ -1,3 +1,26 @@
 require('./style.scss')
 
-document.querySelector('body').className = 'drag';
+let element = document.querySelector('body');
+element.className = 'drag';
+
+const showDragAndDrop = (e) => {
+    e.preventDefault();
+    let classCollection = element.className.split(' ');
+    if (classCollection.indexOf('in-drag') === -1){
+        classCollection.push('in-drag')
+    }
+
+    element.className = classCollection.join(' ');
+}
+const hideDragAndDrop = (e) => {
+    e.preventDefault();
+    let classCollection = element.className.split(' ');
+    let key = classCollection.indexOf('in-drag')
+    if (key >= 0){
+        classCollection.splice(key, 1)
+    }
+    element.className = classCollection.join(' ');
+}
+
+element.addEventListener('dragover', showDragAndDrop)
+element.addEventListener('dragleave', hideDragAndDrop)
