6f90fe03197f5a3a72e8f958713d99d91cd2a584
jos3duardo <jos3duardolopes@gmail.com>
2020-05-04 00:24:16 -0400

    Compressing files for production

--- webpack.prod.config.js
+++ webpack.prod.config.js
@@ -1,4 +1,11 @@
 const merge = require('webpack-merge')
 const webpackBase = require('./webpack.config')
+const CompressionWebPackPlugin = require('compression-webpack-plugin')
 
-module.exports = merge(webpackBase, {})
+module.exports = merge(webpackBase, {
+    plugin: [
+        new CompressionWebPackPlugin({
+            test: /\.js/
+        })
+    ]
+})
